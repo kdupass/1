3c1d9bed2f8ceaa8489b0a76d7494f87eec80ef3
kdupass <59564049+kdupass@users.noreply.github.com>
2020-02-10 18:37:56 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const endpoint = "https://gist.githubusercontent.com/kdupass/692ca7d156bd76f49fe94e0e8f90baf3/raw/bf6dcb3ec3762199df9cbc8f04af50c8df55a5a1/test.json";
+const endpoint = "https://gist.githubusercontent.com/kdupass/692ca7d156bd76f49fe94e0e8f90baf3/raw/ea6d78dcbec2c3404546359e1a2a768bdee69fda/test.json";
 
 const items = [];
 
