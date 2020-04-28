8a2e0ea2518c445eb4ec203dc279549bed3fbda4
kdupass <59564049+kdupass@users.noreply.github.com>
2020-04-28 15:47:49 +0900

    Create ckbox_script.js

---
+++ js/ckbox_script.js
@@ -0,0 +1,13 @@
+
+function checkBox() {
+var check_Box = document.getElementsByName("ckbox"); //체크박스 name
+for (j = 0; j < check_Box.length; j++) {
+ var disign = document.getElementById([j+1]); // 보여질내용
+ if(check_Box[j].checked == true ){
+  disign.style.display = "block";
+ }else{
+  check_Box[j].checked = false;
+  disign.style.display = "none";
+ }
+ }
+}
