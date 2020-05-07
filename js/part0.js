2d2d46796282f498900e463c2347ee014e8ae49f
kdupass <59564049+kdupass@users.noreply.github.com>
2020-05-07 15:24:50 +0900

    Update part0.js

--- js/part0.js
+++ js/part0.js
@@ -3,14 +3,14 @@ $(document).ready(function(){
     var part0 = [];
 
     $.each(data, function(i, quest){
-      console.log(quest.p0.length);
-      for(var num = 0; num < quest.p0.length; num++){
-        part0.push('<tr><td>'+quest.p0[num].area+'</td>');
-        part0.push('<td>'+quest.p0[num].pquest+'</td>');
-        part0.push('<td>'+quest.p0[num].pid+'</td>');
-        part0.push('<td>'+quest.p0[num].cquest+'</td>');
-        part0.push('<td>'+quest.p0[num].cid+'</td>');
-        part0.push('<td>'+quest.p0[num].job+'</td></tr>');
+      console.log(quest.length);
+      for(var num = 0; num < quest.length; num++){
+        part0.push('<tr><td>'+quest[num].area+'</td>');
+        part0.push('<td>'+quest[num].pquest+'</td>');
+        part0.push('<td>'+quest[num].pid+'</td>');
+        part0.push('<td>'+quest[num].cquest+'</td>');
+        part0.push('<td>'+quest[num].cid+'</td>');
+        part0.push('<td>'+quest[num].job+'</td></tr>');
       }
     });
     $('#q0_table').html(html.join(''));
