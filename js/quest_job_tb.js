abd183da43d26671dd916ac636fc2ca843505f13
kdupass <59564049+kdupass@users.noreply.github.com>
2020-05-07 11:57:20 +0900

    Update quest_job_tb.js

--- js/quest_job_tb.js
+++ js/quest_job_tb.js
@@ -1,24 +1,24 @@
 // 2번 테이블 코드//
-function proc(result){
-  $.each(result, function (index, item) {
+function proc(result1){
+  $.each(result1, function (index1, job1) {
 
 // tbody에 출력
 
-    var str = '<tr><td>'+item.area+'</td>';
-      str += '<td>' + item.pquest + '</td>';
-      str += '<td>' + item.pid + '</td>';
-      str += '<td>' + item.cquest + '</td>';
-      str += '<td>' + item.cid + '</td>'
-      str += '<td>' + item.job + '</td></tr>';
-      $("#job1").append(str);
+    var tb = '<tr><td>'+job1.area+'</td>';
+      tb += '<td>' + job1.pquest + '</td>';
+      tb += '<td>' + job1.pid + '</td>';
+      tb += '<td>' + job1.cquest + '</td>';
+      tb += '<td>' + job1.cid + '</td>'
+      tb += '<td>' + job1.job + '</td></tr>';
+      $("#job1").append(tb);
   });
 };
 // json 파일 불러오기
 $(document).ready(function() {
     $.ajax({
       url: './json/q1-tb.json',
-      success : function (result) {
-        proc(result);
+      success : function (result1) {
+        proc(result1);
       }
     });
 });
