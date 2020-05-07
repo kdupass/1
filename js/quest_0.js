abe8f984f99641c0cf4460292acbd08b3a73ad40
kdupass <59564049+kdupass@users.noreply.github.com>
2020-05-07 11:07:29 +0900

    Update quest_0.js

--- js/quest_0.js
+++ js/quest_0.js
@@ -1,8 +1,10 @@
-<!-- 코드의 양이 많아서 result값 받아서 실행하는 proc(result) 함수 생성 -->
+
+// 1번 테이블 코드 //
+// 코드의 양이 많아서 result값 받아서 실행하는 proc(result) 함수 생성
 function proc(result){
   $.each(result, function (index, item) {
 
-    <!-- tbody에 출력 -->
+// tbody에 출력
 
     var str = '<tr><td>'+item.area+'</td>';
       str += '<td>' + item.pquest + '</td>';
@@ -13,10 +15,36 @@ function proc(result){
       $("#q0_table").append(str);
   });
 };
-<!-- json 파일 불러오기-->
+// json 파일 불러오기
 $(document).ready(function() {
     $.ajax({
       url: './json/q0.json',
+      success : function (result) {
+        proc(result);
+      }
+    });
+});
+// 1번 테이블 코드 종료 //
+
+// 2번 테이블 코드//
+function proc(result){
+  $.each(result, function (index, item) {
+
+// tbody에 출력
+
+    var str = '<tr><td>'+item.area+'</td>';
+      str += '<td>' + item.pquest + '</td>';
+      str += '<td>' + item.pid + '</td>';
+      str += '<td>' + item.cquest + '</td>';
+      str += '<td>' + item.cid + '</td>'
+      str += '<td>' + item.job + '</td></tr>';
+      $("#job1").append(str);
+  });
+};
+// json 파일 불러오기
+$(document).ready(function() {
+    $.ajax({
+      url: './json/q1-tb.json',
       success : function (result) {
         proc(result);
       }
