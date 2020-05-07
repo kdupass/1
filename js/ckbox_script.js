02ae30b246b29f8a78b2d761cdbbf6a3f95a32eb
kdupass <59564049+kdupass@users.noreply.github.com>
2020-05-07 11:10:25 +0900

    Update ckbox_script.js

--- js/ckbox_script.js
+++ js/ckbox_script.js
@@ -1,4 +1,4 @@
-
+// 좌측 사이드 체크박스
 function checkBox() {
 var check_Box = document.getElementsByName("ckbox"); //체크박스 name
 for (j = 0; j < check_Box.length; j++) {
@@ -12,6 +12,7 @@ for (j = 0; j < check_Box.length; j++) {
 };
 };
 
+// 직업 퀘스트 체크박스
 function checkjob() {
 var check_job = document.getElementsByName("ckjob"); //체크박스 name
 for (k = 0; k < check_job.length; k++) {
