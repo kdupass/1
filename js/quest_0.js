5021e0a14c5e8e347460560253b2b6f660fbbaf2
kdupass <59564049+kdupass@users.noreply.github.com>
2020-05-08 15:14:48 +0900

    Update quest_0.js

--- js/quest_0.js
+++ js/quest_0.js
@@ -53,3 +53,30 @@ $(document).ready(function() {
       }
     });
 });
+// 2번 테이블 코드 종료 //
+
+// 3번 테이블 코드//
+function proc3(result){
+  $.each(result, function (index, item) {
+
+// tbody에 출력
+
+    var tb = '<tr><td>'+item.area+'</td>';
+      tb += '<td>' + item.pquest + '</td>';
+      tb += '<td>' + item.pid + '</td>';
+      tb += '<td>' + item.cquest + '</td>';
+      tb += '<td>' + item.cid + '</td>'
+      tb += '<td>' + item.job + '</td></tr>';
+      $("#job2").append(tb);
+  });
+};
+// json 파일 불러오기
+$(document).ready(function() {
+    $.ajax({
+      url: './json/q1-sw.json',
+      async:false,
+      success : function (result) {
+        proc3(result);
+      }
+    });
+});
