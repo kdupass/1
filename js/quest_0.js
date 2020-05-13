10e65ec6de92ade68e9bab51c519e81896776974
kdupass <59564049+kdupass@users.noreply.github.com>
2020-05-13 16:33:42 +0900

    Update quest_0.js

--- js/quest_0.js
+++ js/quest_0.js
@@ -261,3 +261,29 @@ $(document).ready(function() {
     });
 });
 // 이화 1~3장 퀘스트 코드 종료 //
+// 신도 1~3장 퀘스트 코드//
+function proc410q4(result){
+  $.each(result, function (index, item) {
+
+// tbody에 출력
+
+    var q4 = '<tr><td>'+item.quest4.area+'</td>';
+      q4 += '<td>' + item.quest4.pquest + '</td>';
+      q4 += '<td>' + item.quest4.pid + '</td>';
+      q4 += '<td>' + item.quest4.cquest + '</td>';
+      q4 += '<td>' + item.quest4.cid + '</td>'
+      q4 += '<td>' + item.quest4.job + '</td></tr>';
+      $("#t410q4").append(q4);
+  });
+};
+// json 파일 불러오기
+$(document).ready(function() {
+    $.ajax({
+      url: './json/q4~q10.json',
+      async:false,
+      success : function (result) {
+        proc410q4(result);
+      }
+    });
+});
+// 신도 1~3장 퀘스트 코드 종료 //
