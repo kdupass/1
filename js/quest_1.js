21bbac8d77054ededaf90d0d0c18c226a42eb1df
kdupass <59564049+kdupass@users.noreply.github.com>
2020-06-03 14:44:33 +0900

    Update quest_1.js

--- js/quest_1.js
+++ js/quest_1.js
@@ -1,20 +1,16 @@
 // 4~10 [4장] 퀘스트 코드//
 function proc410q4(result){
-  var q410 = $.ajax({
-    dataType: "json",
-    url: './json/q4-q10.json',
-    data: data
+
   $.each(result, function (index, item) {
 // tbody에 출력
-    var q4 = '<tr><td>'+item.q410.quest4[0].area +'</td>';
-      q4 += '<td>' + item.q410.quest4[0].pquest + '</td>';
-      q4 += '<td>' + item.q410.quest4[0].pid + '</td>';
-      q4 += '<td>' + item.q410.quest4[0].cquest + '</td>';
-      q4 += '<td>' + item.q410.quest4[0].cid + '</td>'
-      q4 += '<td>' + item.q410.quest4[0].job + '</td></tr>';
+    var q4 = '<tr><td>'+item.q410.quest4.area +'</td>';
+      q4 += '<td>' + item.q410.quest4.pquest + '</td>';
+      q4 += '<td>' + item.q410.quest4.pid + '</td>';
+      q4 += '<td>' + item.q410.quest4.cquest + '</td>';
+      q4 += '<td>' + item.q410.quest4.cid + '</td>'
+      q4 += '<td>' + item.q410.quest4.job + '</td></tr>';
       $("#t410q4").append(q4);
     };
-  });
   });
 };
 // json 파일 불러오기
