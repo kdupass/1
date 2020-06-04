b3ebb32963e0c31c1ae67e0bd1e50e46fd4a264c
kdupass <59564049+kdupass@users.noreply.github.com>
2020-06-04 15:28:23 +0900

    Update ckbox_script.js

--- js/ckbox_script.js
+++ js/ckbox_script.js
@@ -13,15 +13,15 @@ for (j = 0; j < check_Box.length; j++) {
 };
 
 // 직업 퀘스트 체크박스
-function checkjob() {
-var check_job = document.getElementsByName("ckjob"); //체크박스 name
-for (k = 0; k < check_job.length; k++) {
-  var disign = document.getElementById("j"+[k+1]); // 보여질내용
-  if(check_job[k].checked == true ){
-    disign.style.display = "block";
-  }else{
-    check_job[k].checked = false;
-    disign.style.display = "none";
-};
-};
-};
+//function checkjob() {
+//var check_job = document.getElementsByName("ckjob"); //체크박스 name
+//for (k = 0; k < check_job.length; k++) {
+//  var disign = document.getElementById("j"+[k+1]); // 보여질내용
+//  if(check_job[k].checked == true ){
+//    disign.style.display = "block";
+//  }else{
+//    check_job[k].checked = false;
+//    disign.style.display = "none";
+//};
+//};
+//};
