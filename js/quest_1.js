12a188503562d5e3d4d0d5c3c9216c3fc20dee48
kdupass <59564049+kdupass@users.noreply.github.com>
2020-06-04 11:29:02 +0900

    Update quest_1.js

--- js/quest_1.js
+++ js/quest_1.js
@@ -3,12 +3,12 @@ function proc410q4(result){
 
   $.each(result, function (index, item) {
 // tbody에 출력
-    var q4 = '<tr><td>'+item.q410.quest4.area +'</td>';
-      q4 += '<td>' + item.q410.quest4.pquest + '</td>';
-      q4 += '<td>' + item.q410.quest4.pid + '</td>';
-      q4 += '<td>' + item.q410.quest4.cquest + '</td>';
-      q4 += '<td>' + item.q410.quest4.cid + '</td>'
-      q4 += '<td>' + item.q410.quest4.job + '</td></tr>';
+    var q4 = '<tr><td>'+item.area +'</td>';
+      q4 += '<td>' + item.pquest + '</td>';
+      q4 += '<td>' + item.pid + '</td>';
+      q4 += '<td>' + item.cquest + '</td>';
+      q4 += '<td>' + item.cid + '</td>'
+      q4 += '<td>' + item.job + '</td></tr>';
       $("#t410q4").append(q4);
     };
   });
@@ -23,4 +23,23 @@ $(document).ready(function() {
       }
     });
 });
-// 4~10 [4장] 퀘스트 코드 종료 //
+<!-- 검색 기능-->
+<!-- 첫페이지 로딩을 최소화 하기 위하여 click으로 설정 -->
+    $("#t410 > tbody").hide();  <!-- 첫페이지 DB 로딩 시간 절약을 위하여 숨김 처리 -->
+    $("#ck410q1").click(function() {
+      $("#t410q4 > tbody").show(); <!-- 라디오버튼 선택 시 DB 노출 -->
+        var a = $(this).val();
+        $("#t410 > tbody > tr").hide();
+        var tempq410 = $("#cheat-table > tbody > tr > td:nth-child(4n+1):contains('" + a + "')");
+
+        $(tempq410).parent().show();
+    });
+    $("#ck410q2").click(function() {
+      $("#t410q4 > tbody").show(); <!-- 라디오버튼 선택 시 DB 노출 -->
+        var a = $(this).val();
+        $("#t410 > tbody > tr").hide();
+        var tempq410 = $("#cheat-table > tbody > tr > td:nth-child(4n+1):contains('" + a + "')");
+
+        $(tempq410).parent().show();
+    });
+});
