91cdd80baafd715011a201127d4d4016630eb32c
kdupass <59564049+kdupass@users.noreply.github.com>
2020-06-08 13:39:47 +0900

    Update ckbox_script.js

--- js/ckbox_script.js
+++ js/ckbox_script.js
@@ -11,3 +11,32 @@ for (j = 0; j < check_Box.length; j++) {
 };
 };
 };
+
+
+// 서장 퀘스트 코드 //
+// 코드의 양이 많아서 result값 받아서 실행하는 proc(result) 함수 생성
+function proc1(result){
+  $.each(result, function (index, item) {
+
+// tbody에 출력
+
+    var str = '<tr><td>'+item.area+'</td>';
+      str += '<td>' + item.pquest + '</td>';
+      str += '<td>' + item.pid + '</td>';
+      str += '<td>' + item.cquest + '</td>';
+      str += '<td>' + item.cid + '</td>'
+      str += '<td>' + item.job + '</td></tr>';
+      $("#q0_table").append(str);
+    });
+  };
+
+// json 파일 불러오기
+$(document).ready(function(result) {
+  $.ajax({
+    url: './json/q0.json',
+    success : function (result) {
+      proc1(result);
+    };
+});
+});
+// 서장 퀘스트 코드 종료 //
