b3a02e0918aebc8257193b1959fa2d3ad9b50279
kdupass <59564049+kdupass@users.noreply.github.com>
2020-06-29 11:43:06 +0900

    Update q-equip.js

--- js/q-equip.js
+++ js/q-equip.js
@@ -4,19 +4,18 @@ function proc(result){
 
     <!-- tbody에 출력 -->
 
-    var q-equip = '<tr><td>'+item.type+'</td>';
-      q-equip += '<td>' + item.qid + '</td>';
-      q-equip += '<td>' + item.qname + '</td>';
-      q-equip += '<td>' + item.level + '</td>';
-      q-equip += '<td>' + item.rating + '</td>';
-      q-equip += '<td>' + item.bind + '</td>';
-      q-equip += '<td>' + item.etc + '</td></tr>';
-      $("#q-equip-table").append(q-equip);
+    var qequip = '<tr><td>'+item.type+'</td>';
+      qequip += '<td>' + item.qid + '</td>';
+      qequip += '<td>' + item.qname + '</td>';
+      qequip += '<td>' + item.level + '</td>';
+      qequip += '<td>' + item.rating + '</td>';
+      qequip += '<td>' + item.bind + '</td>';
+      qequip += '<td>' + item.etc + '</td></tr>';
+      $("#q-equip-table").append(qequip);
   });
 };
 <!-- json 파일 불러오기-->
 $(document).ready(function() {
-  $(#container).load(function() {
     $.ajax({
       url: './json/q-equip.json',
       success : function (result) {
