0b5c608670db1d0b7e2abf284356a4f9b37703e9
kdupass <59564049+kdupass@users.noreply.github.com>
2020-11-04 10:22:58 +0900

    1-5

---
+++ sky_v.1/nav.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+
+    document.getElementById("nav").innerHTML='<object type="text/html" data="nav.html"></object>';
+
+})
--- sky_v.1/topmenu.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-
-    document.getElementById("topmenu").innerHTML='<object type="text/html" data="topmenu.html"></object>';
-
-})
